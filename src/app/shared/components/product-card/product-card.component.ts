cf4b5af7cd93941a35a9ceb221c4f234c8be2057
Miroslav Mirkovic <mmirkovic@griddynamics.com>
2023-12-31 13:13:58 +0100

    (Updated) product-carousel.component.ts - The carousel functionality has been added.

--- src/app/shared/components/product-card/product-card.component.ts
+++ src/app/shared/components/product-card/product-card.component.ts
@@ -1,4 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,12 +13,26 @@ import { Router } from '@angular/router';
   templateUrl: './product-card.component.html',
   styleUrls: ['./product-card.component.scss'],
 })
-export class ProductCardComponent implements OnInit {
+export class ProductCardComponent implements OnInit, AfterViewInit {
+  private totalSize!: number;
   @Input() product!: any;
+  @ViewChild('productElement', { static: true }) productElement!: ElementRef;
 
   constructor(private router: Router) {}
 
   ngOnInit(): void {}
+  ngAfterViewInit() {
+    const nativeElement = this.productElement.nativeElement;
+    const styles = window.getComputedStyle(nativeElement);
+    const marginLeft = parseInt(styles.marginLeft);
+    const marginRight = parseInt(styles.marginRight);
+    const cardWidth = this.productElement.nativeElement.offsetWidth;
+    this.totalSize = cardWidth + marginLeft + marginRight;
+  }
+
+  getWidth(): number {
+    return this.totalSize;
+  }
 
   navigateProduct(id: number) {
     this.router.navigate([`product/${id}`]);
--- src/app/shared/components/product-carousel/product-carousel.component.ts
+++ src/app/shared/components/product-carousel/product-carousel.component.ts
@@ -7,6 +7,7 @@ import {
   ViewChild,
   ViewChildren,
 } from '@angular/core';
+import { ProductCardComponent } from '../product-card/product-card.component';
 
 @Component({
   selector: 'app-product-carousel',
@@ -14,16 +15,39 @@ import {
   styleUrls: ['./product-carousel.component.scss'],
 })
 export class ProductCarouselComponent implements OnInit {
+  private cardWidth!: number;
+  counter: number = 0;
   @Input() products!: any[];
-  @ViewChild('slider', { static: true }) slider!: ElementRef;
-  @ViewChildren('cards') cards!: QueryList<ElementRef>;
+  @ViewChild('slider', { static: false }) slider!: ElementRef;
+  @ViewChild('left', { static: false }) left!: ElementRef;
+  @ViewChildren(ProductCardComponent) cards!: QueryList<ProductCardComponent>;
+
+  disabledButtons: boolean[] = [false, true];
 
   constructor() {}
 
-  slideLeft() {
-    console.log(this.slider.nativeElement);
-    this.cards.forEach((card: ElementRef) => console.log(card));
+  ngOnInit(): void {}
+
+  handleClick(event: any) {
+    const button = event.target.closest('custom-button');
+    if (!button) return;
+    this.cardWidth = this.cards.first.getWidth();
+    const action: string = button.dataset.action;
+    this.counter = action.includes('left')
+      ? this.counter + 1
+      : this.counter - 1;
+    this.slide(action, this.slider, this.counter);
   }
 
-  ngOnInit(): void {}
+  private slide(action: string, slider: ElementRef, counter: number) {
+    const direction = action.includes('left') ? 1 : -1;
+    slider.nativeElement.scrollLeft += direction * this.cardWidth;
+    this.updateButtons(counter, 3);
+  }
+
+  private updateButtons(counter: number, cardsPerView: number) {
+    const isAtStart = counter === 0;
+    const isAtEnd = this.cards.length - cardsPerView === counter;
+    this.disabledButtons = [isAtEnd, isAtStart];
+  }
 }
