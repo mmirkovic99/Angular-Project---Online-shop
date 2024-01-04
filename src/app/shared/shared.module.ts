c7dd1cb60028de56aee313e5a7c62248ea06d3ef
Miroslav Mirkovic <mmirkovic@griddynamics.com>
2024-01-04 22:30:09 +0100

    (Added) sale.pipe.ts - In this file, a custom pipe named sale has been implemented. This pipe is used to checked whether is the value equal to 'On Sale' or not

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -15,6 +15,7 @@ import { FavoriteButtonComponent } from './components/favorite-button/favorite-b
 import { ProfileButtonComponent } from './components/profile-button/profile-button.component';
 import { CustomSelectComponent } from './components/custom-select/custom-select.component';
 import { TagPipe } from '../pipes/tag.pipe';
+import { SalePipe } from '../pipes/sale.pipe';
 
 @NgModule({
   declarations: [
@@ -32,6 +33,7 @@ import { TagPipe } from '../pipes/tag.pipe';
     ProfileButtonComponent,
     CustomSelectComponent,
     TagPipe,
+    SalePipe,
   ],
   imports: [CommonModule, ReactiveFormsModule],
   exports: [
@@ -49,6 +51,7 @@ import { TagPipe } from '../pipes/tag.pipe';
     ProfileButtonComponent,
     CustomSelectComponent,
     TagPipe,
+    SalePipe,
   ],
 })
 export class SharedModule {}
