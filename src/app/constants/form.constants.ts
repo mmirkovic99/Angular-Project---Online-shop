55e347789b4dbddc230b5809a5900dcda8fdedb7
Miroslav Mirkovic <mmirkovic@griddynamics.com>
2024-01-13 10:03:35 +0100

    (Updated) custom-input.component.ts - An error message will be displayed if any field is not filled in correctly during user login and registration.

---
+++ src/app/constants/form.constants.ts
@@ -0,0 +1,8 @@
+export enum FormFields {
+  name = 'Name',
+  surname = 'Surname',
+  email = 'Email',
+  username = 'Username',
+  password = 'Password',
+  confirmPassword = 'Confirm password',
+}
--- src/app/shared/components/custom-input/custom-input.component.ts
+++ src/app/shared/components/custom-input/custom-input.component.ts
@@ -1,15 +1,62 @@
-import { Component, Input, OnChanges, OnInit } from '@angular/core';
+import { Component, DoCheck, Input, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { FormFields } from 'src/app/constants/form.constants';
+
 @Component({
   selector: 'custom-input',
   templateUrl: './custom-input.component.html',
   styleUrls: ['./custom-input.component.scss'],
 })
-export class CustomInputComponent implements OnInit {
+export class CustomInputComponent implements OnInit, DoCheck {
   @Input() label!: string;
   @Input() type!: 'text' | 'password';
   @Input() control: FormControl = new FormControl();
   @Input() error?: boolean;
 
+  @Input() passwordNoMatch?: boolean;
+
+  errorMessage: string = '';
+
+  ngDoCheck(): void {
+    if (this.error) this.showError(this.label);
+    else this.errorMessage = '';
+  }
+
   ngOnInit(): void {}
+
+  private showError(label: string) {
+    switch (label) {
+      case FormFields.name:
+      case FormFields.surname:
+      case FormFields.username:
+        this.errorMessage = `${this.label} is required field.`;
+        break;
+      case FormFields.email:
+        this.handleEmailError(this.control, FormFields.email);
+        break;
+      case FormFields.password:
+        this.handlePasswordError(this.control, FormFields.password);
+        break;
+      default: // Confirm Password
+        this.handleConfirmPassword(this.control, FormFields.confirmPassword);
+    }
+  }
+
+  private handlePasswordError(control: FormControl, label: string): void {
+    if (!control.dirty) this.errorMessage = `${label} is required field.`;
+    else if (!control.valid)
+      this.errorMessage = `${label} is invalid. ${label} must contain at least one uppercase letter and one special character, and it must be a minimum of 8 characters long.`;
+  }
+
+  private handleEmailError(control: FormControl, label: string): void {
+    if (!control.dirty) this.errorMessage = `${label} is required field.`;
+    else if (!control.valid) this.errorMessage = `${label} is invalid.`;
+  }
+
+  private handleConfirmPassword(control: FormControl, label: string) {
+    console.log('conf pass');
+    if (!control.dirty) this.errorMessage = `${label} is required field.`;
+    else if (this.passwordNoMatch)
+      this.errorMessage = `Password and ${label} fields do not match.`;
+  }
 }
